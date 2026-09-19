a6c37b91edb200f00b913022af5b8973482d2d79
DesynchVT <desynchvt@proton.me>
2026-09-19 13:56:01 +0200

    Added controls to all settings

--- src/settings.ts
+++ src/settings.ts
@@ -12,7 +12,7 @@ export interface RaPluginSettings {
 export const DEFAULT_SETTINGS: RaPluginSettings = {
 	raWebApiKey: '',
 	propertiesAsLinks: true,
-	raUsername: "Desynch",
+	raUsername: "",
 	raGamesPath: "RetroAchievements/library",
 	includeSubsets: false,
 };
@@ -27,11 +27,20 @@ export class RaSettingTab extends PluginSettingTab {
 
 	display(): void {
 		const { containerEl } = this;
-
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Ra web API key')
+			.setName("RA username")
+			.setDesc("Your username on retroachievements.org")
+			.addText((text) => {
+				text.setValue(this.plugin.settings.raUsername).onChange(async (val) => {
+					this.plugin.settings.raUsername = val;
+					await this.plugin.saveSettings();
+				})
+			});
+		new Setting(containerEl)
+			.setName('RA web API key')
+			.setDesc("Your personal API key from retroachievements.org.\nFound under settings.")
 			.addComponent(el => new SecretComponent(this.app, el)
 				.setValue(this.plugin.settings.raWebApiKey)
 				.onChange(async (val) => {
@@ -39,20 +48,32 @@ export class RaSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				})
 			);
-		new Setting(containerEl).setName("Properties as links").setDesc("Save game metadata as internal links in your vault to create connections between them").addToggle(btn => {
-			btn.setValue(this.plugin.settings.propertiesAsLinks).onChange(async (value) => {
-				this.plugin.settings.propertiesAsLinks = value;
-				await this.plugin.saveSettings();
-			})
-		});
 		new Setting(containerEl)
-			.setName("Ra username")
-			.setDesc("Your username on retroachievements.org")
-			.addText((text) => {
-				text.setValue(this.plugin.settings.raUsername).onChange(async (val) => {
-					this.plugin.settings.raUsername = val;
+			.setName("Properties as links")
+			.setDesc(`Save game metadata as internal links in your vault to create connections between them.\nDefault: ${DEFAULT_SETTINGS.propertiesAsLinks}`)
+			.addToggle(btn => {
+				btn.setValue(this.plugin.settings.propertiesAsLinks).onChange(async (value) => {
+					this.plugin.settings.propertiesAsLinks = value;
 					await this.plugin.saveSettings();
 				})
-			})
+			});
+		new Setting(containerEl)
+			.setName("Include subsets")
+			.setDesc(`Whether auto import includes subsets or not.\nDefault: ${DEFAULT_SETTINGS.includeSubsets}`)
+			.addToggle(btn => {
+				btn.setValue(this.plugin.settings.includeSubsets).onChange(async (value) => {
+					this.plugin.settings.includeSubsets = value;
+					await this.plugin.saveSettings();
+				})
+			});
+		new Setting(containerEl)
+			.setName("Game note directory")
+			.setDesc(`The path imported RA games are imported to.\nDefault: ${DEFAULT_SETTINGS.raGamesPath}`)
+			.addText((text) => {
+				text.setValue(this.plugin.settings.raGamesPath).onChange(async (val) => {
+					this.plugin.settings.raGamesPath = val;
+					await this.plugin.saveSettings();
+				})
+			});
 	}
 }
