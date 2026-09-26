1314c8365e6ae14ce5efad6a46bc9975b7b0c3b5
DesynchVT <desynchvt@proton.me>
2026-09-26 12:45:49 +0200

    Added glow to card items depending on mastery status

--- src/commands/createBase.ts
+++ src/commands/createBase.ts
@@ -2,6 +2,10 @@ import { stringifyYaml, type BasesConfigFile, type BasesConfigFileView } from 'o
 import RaSearchPlugin from '../main';
 import { noteExists, openVaultNote } from '../utils';
 
+const RA_STATUS_MARKER = `html("<span class='ra-status-value'>" + if(status, status, "none") + "</span><div class='ra-status ra-status-" + if(status, status, "none") + "'></div>")`;
+// const RA_STATUS_MARKER = `html("<span class='ra-status-label'>Status</span><span class='ra-status-value ra-status-" + if(status, status, "none") + "'>" + if(status, status, "none") + "</span>")`;
+
+
 type CardsView = BasesConfigFileView & {
 	image?: string;
 	imageFit?: string;
@@ -12,16 +16,19 @@ type CardsView = BasesConfigFileView & {
 const view: CardsView = {
 	type: "cards",
 	name: "default",
-	order: ["title", "console", "status"],
+	order: ["title", "console", "formula.raStatus"],
 	image: "coverUrl",
 	imageFit: "contain",
 	cardSize: 320,
+	imageAspectRatio: 1.40
 };
 
 export const runCreateBase = async (plugin: RaSearchPlugin) => {
 	const base: BasesConfigFile = {
 		filters: { and: [`file.inFolder("${plugin.settings.raGamesPath}")`, 'category == "RetroAchievements"'] },
+		formulas: { raStatus: RA_STATUS_MARKER, },
 		views: [view],
+		properties: { "formula.raStatus": { displayName: "status" } }
 	};
 	const fileName = `${plugin.settings.raGamesPath}/RA Library.base`;
 	let note = await noteExists(plugin.app, fileName);
